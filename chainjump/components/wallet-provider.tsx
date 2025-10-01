706abc6c2173a2820a8931d79a6afa67027a3c50
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2025-10-01 15:35:27 +0530

    wallet provider/farcaster config

---
+++ chainjump/components/wallet-provider.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { farcasterMiniApp as miniAppConnector } from '@farcaster/miniapp-wagmi-connector'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { http, WagmiProvider, createConfig } from 'wagmi'
+import {
+  celo, 
+  celoAlfajores, 
+} from 'viem/chains'
+
+export const config = createConfig({
+  chains: [celo,celoAlfajores],
+  transports: {
+    [celoAlfajores.id]: http('https://alfajores-forno.celo-testnet.org'),
+    [celo.id]: http('https://forno.celo.org'),
+  },
+  connectors: [miniAppConnector()],
+})
+
+const queryClient = new QueryClient()
+
+export function WalletProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  )
+}
